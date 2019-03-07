63c35c933722b6cd575c1603f7943ee86970715f
Jonathan Peclat <peclatj@bluewin.ch>
2019-03-07 01:32:58 +0100

    First test
    Gravity is controlled with WASD, a small map with some barrel that are affected by the gravity

---
+++ Assets/Scripts/Player/Gravity.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    private float x = 0.0f;
+    private float y = 0.0f;
+    public float intensity = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = 0.0f;
+        y = 0.0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            y = intensity;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            y = -intensity;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            x = -intensity;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            x = intensity;
+        }
+        
+        Physics2D.gravity = new Vector3(x, y, 0.0f);
+    }
+}
